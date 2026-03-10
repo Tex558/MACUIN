29951cd5738d82ca6c8f3f0a2c30065e13c44c54
EJC <122042676@upq.edu.mx>
2026-03-10 17:08:42 -0600

    Correciones flask

--- flask/app.py
+++ flask/app.py
@@ -1,51 +1,50 @@
 from flask import Flask, render_template, redirect, url_for
-# Crea una instancia de la aplicación Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('/auth/LoginU.html')#
+    return render_template('/LoginU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/CrearCuentaU.html')#
+@app.route('/CrearCuentaU')
+def crear_cuenta():
+    return render_template('/CrearCuentaU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/ResumenPedidosU.html')
+@app.route('/ResumenPedidosU')
+def resumen():
+    return render_template('/ResumenPedidosU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/CatalogoU.html')#
+@app.route('/CatalogoU')
+def catalogo():
+    return render_template('/CatalogoU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/ResumenPedidosU.html')#
+@app.route('/ResumenPedidosU')
+def resumenpedidos():
+    return render_template('/ResumenPedidosU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/EstatusPedidoU.html')#
+@app.route('/EstatusPedidoU')
+def estatuspedido():
+    return render_template('/EstatusPedidoU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/PedidosEnCursoU.html')#
+@app.route('/PedidosEnCursoU')
+def pedidosencurso():
+    return render_template('/PedidosEnCursoU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/StockU.html')#
+@app.route('/StockU')
+def stocku():
+    return render_template('/StockU.html')
 
 
-@app.route('/')
-def index():
-    return render_template('/auth/CancelarPedidoU.html')#
+@app.route('/CancelarPedidoU    ')
+def cancelarpedido():
+    return render_template('/CancelarPedidoU.html')
 
 
 if __name__ == "__main__":
